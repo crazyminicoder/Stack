65ba7e77ce78bd40ba397ccd6102038a088a39b7
Syed Wali Uddin Quadri <69212896+crazyminicoder@users.noreply.github.com>
2024-09-22 12:59:44 -0500

    added comments

--- backSpace.py
+++ backSpace.py
@@ -1,3 +1,14 @@
+# Example 1:
+
+# Input: s = "ab#c", t = "ad#c"
+# Output: true
+# Explanation: Both s and t become "ac".
+# Example 2:
+
+# Input: s = "ab##", t = "c#d#"
+# Output: true
+# Explanation: Both s and t become "".
+
 class Solution:
     def backSpace(self, s, t):
         stack1 = []
